69d4ab8eef4b1fe890ffddb9e8d6aee78fc0ac70
Adrian Del Moral <adm@srjingles.com>
2023-08-18 09:30:05 +0200

    Changed the Controller to return the correct view of a blade file instead of just text.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Response;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
         //
-        return response('Hello World!');
+        return view('chirps.index');
     }
 
     /**
